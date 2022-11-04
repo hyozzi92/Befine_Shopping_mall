10e3a55f98e006b2fd27d24a9657dacea40a97c6
hyozzi92 <gytjdzja@gmail.com>
2022-11-04 15:56:31 +0900

    Register Layout

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Register.scss';
+
+import SignupData from './SignupData';
+import SignupData2 from './SignupData2';
 
 const Register = () => {
-  return <div></div>;
-};
+  const [transPage, setTransPage] = useState(false);
 
+  return (
+    <div>
+      {transPage ? (
+        <SignupData2 setTransPage={setTransPage} />
+      ) : (
+        <SignupData setTransPage={setTransPage} />
+      )}
+    </div>
+  );
+};
 export default Register;
